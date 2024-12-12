068412a6a7cb78a1ae5359bbb490629b39dee160
Strad5y <166418890+Strad5y@users.noreply.github.com>
2024-12-12 15:31:21 +0100

    updated css html

---
+++ Frontend/home.js
@@ -0,0 +1,4 @@
+// This file contains the code for the home page of the website
+    document.getElementById('uploadButton').addEventListener('click', function() {
+        window.location.href = "upload.html"; // Replace 'upload.html' with your actual upload page URL
+    });
