27165914edeeadf1a6049c2c8d10392c4a7ff989
Strad5y <166418890+Strad5y@users.noreply.github.com>
2025-01-13 15:10:19 +0100

    add requirments

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return ("podclassAI")
+    return "Hello, Flask!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.", port=5005)
+    app.run(host="0.0.0.0", port=5005)
 
