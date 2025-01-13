110a576858a23b4e4650fdbf5010b404fa190d97
Strad5y <166418890+Strad5y@users.noreply.github.com>
2025-01-13 16:24:34 +0100

    Navigation between home and upload
    Now Buttons can navigate you between home and upload page

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
-# Flask-Instanz erstellen und den Pfad für Templates anpassen
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Rendert die Datei home.html aus dem Ordner templates
     return render_template('home.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5005)
