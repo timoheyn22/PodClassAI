3d68f8b7551994d8377495fc90aa8234bc8a87fc
timoheyn22 <166369125+timoheyn22@users.noreply.github.com>
2025-01-23 14:20:06 +0100

    Also Greyed out the download podcast button and grey it in when activated

--- static/upload.js
+++ static/upload.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Set initial disabled-button class on page load
     createPodcastButton.classList.add('disabled-button');
-
+    downloadPodcastButton.classList.add('disabled-button');
 
     // Event listener for PDF upload button and input
     const pdfInput = document.getElementById('pdfInput');
@@ -74,7 +74,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 .then((data) => {
                     if (data.message) {
                         alert(data.message);
-                        downloadPodcastButton.disabled = false; // Enable the download button
+                        downloadPodcastButton.disabled = false;
+                        createPodcastButton.classList.remove('disabled-button'); // Enable the download button
                     } else if (data.error) {
                         alert('Error: ' + data.error);
                     }
